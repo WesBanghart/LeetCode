99e149dd75c18fbcb2913fcba20ff4f075e95b59
Wes Banghart <Wesley.Banghart@hamiltoncompany.com>
2021-09-22 07:42:42 -0700

    Completed Reverse Integer.

---
+++ P7/CSharp/ReverseInteger/ReverseInteger/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sln = new Solution();
+            int x1 = 123;
+            int x2 = -123;
+            int x3 = 120;
+            int x4 = 0;
+            int x5 = 214748364;
+            int x6 = 1534236469;
+
+            Console.WriteLine(sln.Reverse(x1));
+            Console.WriteLine(sln.Reverse(x2));
+            Console.WriteLine(sln.Reverse(x3));
+            Console.WriteLine(sln.Reverse(x4));
+            Console.WriteLine(sln.Reverse(x5));
+            Console.WriteLine(sln.Reverse(x6));
+
+        }
+    }
+
+    public class Solution {
+        //TODO: This uses too much space, find a better solution that uses less
+        public int Reverse(int x)
+        {
+            if (x < 10 && x > -10) return x;
+
+            long y = 0;
+            long tempx = x > 0 ? x : -x;
+            int xRev = (int) Math.Pow(10, Math.Floor(Math.Log10(tempx)));
+
+            while (tempx != 0)
+            {
+                y += tempx % 10 * xRev;
+                xRev /= 10;
+                tempx /= 10;
+            }
+
+            y = x > 0 ? y : -y;
+            return (y > 2147483647 || y < -2147483648) ? 0 : (int) y;
+        }
+    }
+}
--- P9/CSharp/PalendromeNumber/PalendromeNumber/Program.cs
+++ P9/CSharp/PalendromeNumber/PalendromeNumber/Program.cs
@@ -5,10 +5,10 @@ namespace PalendromeNumber
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             Solution sln = new Solution();
-
             int input1 = 121;
             int input2 = -121;
             int input3 = 10;
@@ -16,16 +16,35 @@ namespace PalendromeNumber
             int input5 = 0;
             int input6 = 1;
             int input7 = 11;
-            int input8 = 12;
+            int input8 = 10;
+            int input9 = 1000000001;
+            //ExecuteWithTimer((() => sln.IsPalindrome(input1)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input2)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input3)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input4)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input5)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input6)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input7)));
+            //ExecuteWithTimer((() => sln.IsPalindrome(input8)));
+            ExecuteWithTimer((() => sln.IsPalindrome(input9)));
+            
+            //Console.WriteLine(sln.IsPalindrome(input2));
+            //Console.WriteLine(sln.IsPalindrome(input3));
+            //Console.WriteLine(sln.IsPalindrome(input4));
+            //Console.WriteLine(sln.IsPalindrome(input5));
+            //Console.WriteLine(sln.IsPalindrome(input6));
+            //Console.WriteLine(sln.IsPalindrome(input7));
+            //Console.WriteLine(sln.IsPalindrome(input8));
+        }
 
-            Console.WriteLine(sln.IsPalindrome(input1));
-            Console.WriteLine(sln.IsPalindrome(input2));
-            Console.WriteLine(sln.IsPalindrome(input3));
-            Console.WriteLine(sln.IsPalindrome(input4));
-            Console.WriteLine(sln.IsPalindrome(input5));
-            Console.WriteLine(sln.IsPalindrome(input6));
-            Console.WriteLine(sln.IsPalindrome(input7));
-            Console.WriteLine(sln.IsPalindrome(input8));
+        private static void ExecuteWithTimer(Func<bool> func)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            Console.WriteLine(func());
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Execution Time: {watch.ElapsedTicks} ticks");
         }
     }
 
@@ -46,6 +65,7 @@ namespace PalendromeNumber
                 xrevPlace /= 10;
                 xtemp /= 10;
             }
+
             return x == y;
         }
     }
